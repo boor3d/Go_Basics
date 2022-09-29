9acd94d950d9dd0b1d525939ae301dcec7ea762a
boor3d <jest3r.boor3d@gmail.com>
2022-09-28 23:16:45 -0500

    Variable Edits

--- Code/lectures/exercise/variables/variables.go
+++ Code/lectures/exercise/variables/variables.go
@@ -36,10 +36,10 @@ func main() {
 	var myAgeDays int
 	myAgeDays = myAge * 365
 
-	// Print
-	fmt.Println("My Favorite Color:", favColor, "\nMy Birth Year and Age:", myBirthYear, "&", myAge)
+	// Print Variables
+	fmt.Println("My Favorite Color:", favColor, "\nMy Birth Year and Age:", myBirthYear, ",", myAge)
 
-	fmt.Println("First Initial:", firstInitial, "\nLast Initial:", lastInitial)
+	fmt.Println("First Initial:", firstInitial, "\nLast Initial", lastInitial)
 
 	fmt.Println("My Age in Days:", myAgeDays)
 
