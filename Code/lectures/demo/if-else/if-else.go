daf7aba40cfe21dbb90f94ee117311797c5685e4
boo_r3d <jest3r.boor3d@gmail.com>
2022-10-04 13:30:25 -0500

    If-else Demo

--- Code/lectures/demo/if-else/if-else.go
+++ Code/lectures/demo/if-else/if-else.go
@@ -2,11 +2,20 @@ package main
 
 import "fmt"
 
-func average(a, b, c int) float32 {
+func average(a, b int) float32 {
 	// Convert the sum of the scores into a float32
-	return float32(a+b+c) / 3
+	return float32(a+b) / 2
 }
 
 func main() {
+	quiz1, quiz2 := 9, 8
+
+	if quiz1 > quiz2 {
+		fmt.Println("Quiz 1 is greater than quiz 2")
+	} else if quiz1 < quiz2 {
+		fmt.Println("Quiz 1 is less than quiz 2")
+	} else {
+		fmt.Println("The quiz scores are equal")
+	}
 
 }
